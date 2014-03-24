532987507eafc7f838e1d31278745293dba13344
Jacek Wysocki <jacek.wysocki@gmail.com>
2014-03-24 21:31:55 +0100

    list exc

--- test/list.js
+++ test/list.js
@@ -1,5 +1,5 @@
-var fb = require('../lib/list');
-require('should');
+var should = require('should');
+var List = require('../lib/list');
 
 /**
  *  Develop a recently-used-list class to hold strings uniquely in Last-In-First-Out order.
@@ -16,28 +16,77 @@ require('should');
 describe('RecentlyUsedList', function () {
     'use strict';
 
+    var list;
     beforeEach(function () {
     });
 
     it('enqueue new items', function () {
+        list = new List();
+        list.enqueue(1)
+        list.dequeue().should.equal(1)
     });
 
     it('adds first things first last things last', function () {
+        list = new List()
+        list.enqueue(1)
+        list.enqueue(2)
+        list.enqueue(3)
+        list.dequeue().should.equal(1)
+        list.dequeue().should.equal(2)
+        list.dequeue().should.equal(3)
     });
 
     it('search items by index', function () {
+        list = new List()
+        list.enqueue(1);
+        list.enqueue(2);
+        list.enqueue(3);
+
+        list[2].should.equal(3);
+        list[0].should.equal(1);
+        list[1].should.equal(2);
+
+        should(list[4]).be.undefined;
     });
 
     it('adds only unique items', function () {
+        list = new List()
 
+        list.enqueue(1);
+        list.enqueue(2);
+        list.enqueue(3);
+        list.enqueue(3);
+        list.enqueue(2);
+        list.enqueue(1);
+
+        list.dequeue().should.equal(3);
+        list.dequeue().should.equal(2);
+        list.dequeue().should.equal(1);
+
+        should(list.dequeue()).be.undefined;
     });
 
     it('allow only not null values', function () {
-
+        list = new List()
+        list.enqueue(1)
+        should(list.enqueue(1)).throw();
     });
 
     it('has capacity', function () {
+        list = new List(3);
 
+        list.enqueue(1)
+        list.enqueue(2)
+        list.enqueue(3)
+        list.enqueue(4)
+        list.enqueue(5)
+        list.enqueue(6)
+
+        list.dequeue().should.equal(4);
+        list.dequeue().should.equal(5);
+        list.dequeue().should.equal(6);
+
+        should(list.dequeue()).be.undefined;
     });
 
 });
